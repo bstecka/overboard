41819c46b65a8f597bbb06265eea2b6c1ed4ac6c
negacja <bstecka@gmail.com>
2018-03-15 11:22:40 +0100

    Add ability to find questions by tags and simple tag page

--- overboard/overboardapp/views.py
+++ overboard/overboardapp/views.py
@@ -1,8 +1,7 @@
  # overboardapp/views.py
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import TemplateView
-from .models import Notification
-from .models import Question
+from .models import Notification, Question, Tag
 import datetime
 
 
@@ -22,17 +21,29 @@ class PageView(TemplateView):
     template_name = "page.html"
 
 
+def tag_list(request):
+    tags = Tag.objects.all().order_by('tag_name')
+    return render(request, 'tags.html', {'tags': tags})
+
+
 def latest_question_list(request):
     latest_questions = Question.objects.all().order_by('-pub_date')[:10]
-    return render(request, 'index_content.html', {'questions': latest_questions, 'selected_tab': 'last'});
+    return render(request, 'index_content.html', {'questions': latest_questions, 'selected_tab': 'last'})
 
 
 def topweek_question_list(request):
     from_date = datetime.datetime.now() - datetime.timedelta(days=7)
     latest_questions = Question.objects.filter(pub_date__range=[from_date, datetime.datetime.now()]).order_by('-pub_date')[:10]
-    return render(request, 'index_content.html', {'questions': latest_questions, 'selected_tab': 'week'});
+    return render(request, 'index_content.html', {'questions': latest_questions, 'selected_tab': 'week'})
 
 
-def question_detail(request, question_id):   # Page with deatils of question
+def question_detail(request, question_id):   # Page with details of question
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'question_detail.html', {'question': question})
+
+
+def tag_page(request, tag_id):
+    tag = get_object_or_404(Tag, pk=tag_id)
+    questions = Question.objects.filter(tag=tag)
+    return render(request, 'tag_page.html', {'tag': tag, 'questions': questions})
+
