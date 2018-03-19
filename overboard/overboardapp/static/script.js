f9de644b3a700ef7fc10aa4d2bf8c7df07f542de
negacja <bstecka@gmail.com>
2018-03-19 14:31:33 +0100

    WIP Enable voting on questions

---
+++ overboard/overboardapp/static/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    document.getElementById("upvote").addEventListener("click", function() {
+        alert("upvote");
+    }, false);
+
+    document.getElementById("downvote").addEventListener("click", function() {
+        alert("downvote");
+    }, false);
+});
